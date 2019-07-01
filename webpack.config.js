0e7bff496f7c890467a1a8810d26e3ba085b6619
frankfeng <17717444584@163.com>
2019-07-01 23:46:50 +0800

    初识webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist/js/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // 将 JS 字符串生成为 style 节点
+                },
+                {
+                    loader: "css-loader", options: { importLoaders: 1 } // 将 CSS 转化成 CommonJS 模块
+                }, {
+                    loader: 'postcss-loader',
+                },
+                {
+                    loader: "sass-loader" // 将 Sass 编译成 CSS
+                }]
+            }
+        ]
+
+    }
+};
